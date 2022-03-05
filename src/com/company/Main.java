278615fd461a4194be39a07346762f8d265d5400
Suresh Lama <sureshlama@Sureshs-MacBook-Pro.local>
2022-03-05 16:06:57 +0900

    K xa pasa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,19 @@
 package com.company;
-
+//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
 public class Main {
     public static void main(String[] args) {
         int highScorePosition =calculateHighScorePosition(50);
         displayHighScorePosition("Rez",highScorePosition);
-
+//        git remote set-url origin https://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@github.com/Rezthecoder/JavaProect.git
     }
-
+    //https://github.com/Rezthecoder/JavaProect.git
 
     public static void displayHighScorePosition(String Player, int HighScorePosition) {
         System.out.println("Name " + Player + "Position:" + HighScorePosition);
     }
     public static int calculateHighScorePosition(int score) {
         if (score >= 1000) {
-            return 1;
+            return 1;Applegirl1;
         } else if ((score >= 500) && (score < 1000)) {
             return 2;
         } else if ((score >= 100) && (score < 500)){
